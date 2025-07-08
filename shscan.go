9b1e1b61fc3c08e87b1c31adc452e968e3582ebf
Jakub Jarosz <jakub@jarosz.dev>
2025-07-08 08:04:20 +0100

    parse network address
    Signed-off-by: Jakub Jarosz <jakub@jarosz.dev>

--- shscan.go
+++ shscan.go
@@ -1,1 +1,39 @@
 package shscan
+
+import (
+	"encoding/binary"
+	"fmt"
+	"net"
+)
+
+func VerifySocket(addr string) (bool, error) {
+	ipAddr, err := net.ResolveIPAddr("ip4", addr)
+	if err != nil {
+		return false, fmt.Errorf("netscan: resolving IP Address %w", err)
+	}
+	conn, err := net.ListenIP("ip4:tcp", ipAddr)
+	if err != nil {
+		return false, fmt.Errorf("netscan: creating connection %w", err)
+	}
+	conn.Close()
+	return true, nil
+}
+
+func GenerateHostRange(network string) ([]string, error) {
+	_, ipv4Net, err := net.ParseCIDR(network)
+	if err != nil {
+		return nil, fmt.Errorf("netscan: generating host range %w", err)
+	}
+
+	mask := binary.BigEndian.Uint32(ipv4Net.Mask)
+	start := binary.BigEndian.Uint32(ipv4Net.IP)
+	finish := (start & mask) | (mask ^ 0xffffffff)
+
+	var hosts []string
+	for i := start + 1; i <= finish-1; i++ {
+		ip := make(net.IP, 4)
+		binary.BigEndian.PutUint32(ip, i)
+		hosts = append(hosts, ip.String())
+	}
+	return hosts, nil
+}
--- shscan_test.go
+++ shscan_test.go
@@ -1,1 +1,34 @@
 package shscan_test
+
+import (
+	"testing"
+
+	"github.com/google/go-cmp/cmp"
+	"github.com/qba73/shscan"
+)
+
+func TestGenerateHostsFromNetAddress(t *testing.T) {
+	t.Parallel()
+
+	ipaddresses, err := shscan.GenerateHostRange("192.168.1.0/24")
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	want := 254
+	got := len(ipaddresses)
+
+	if !cmp.Equal(want, got) {
+		t.Error(cmp.Diff(want, got))
+	}
+}
+
+func TestGenerateHostsFailsOnInvalidNetAddress(t *testing.T) {
+	t.Parallel()
+
+	netAddress := "192.168.1.0/45"
+	_, err := shscan.GenerateHostRange(netAddress)
+	if err == nil {
+		t.Errorf("want err on invalid address %s, got nil", netAddress)
+	}
+}
